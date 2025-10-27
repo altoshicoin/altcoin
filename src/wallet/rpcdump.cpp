3a2bd5ad872f09f5e907355436e789e495c139ab
ALTCOIN Dev <dev@altcoin.local>
2025-10-27 20:03:27 +0700

    branding(tier1): error text "Invalid Altcoin address" in dumpprivkey

--- src/wallet/rpcdump.cpp
+++ src/wallet/rpcdump.cpp
@@ -701,7 +701,7 @@ RPCHelpMan dumpprivkey()
     std::string strAddress = request.params[0].get_str();
     CTxDestination dest = DecodeDestination(strAddress);
     if (!IsValidDestination(dest)) {
-        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid Litecoin address");
+        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid Altcoin address");
     }
     auto keyid = GetKeyForDestination(spk_man, dest);
     if (keyid.IsNull()) {
