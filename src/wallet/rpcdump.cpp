524d2a0f24fcd636999c30bc3349813f99cc633d
ALTCOIN Dev <dev@altcoin.local>
2025-10-28 17:29:02 +0700

    branding: finalize dump header/messages in rpcdump

--- src/wallet/rpcdump.cpp
+++ src/wallet/rpcdump.cpp
@@ -1,3 +1,5 @@
+#include <config/bitcoin-config.h>
+#include <clientversion.h>
 // Copyright (c) 2009-2020 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
@@ -785,7 +787,7 @@ RPCHelpMan dumpwallet()
     std::sort(vKeyBirth.begin(), vKeyBirth.end());
 
     // produce output
-    file << strprintf("# Wallet dump created by Altcoin %s\n", CLIENT_BUILD);
+    file << strprintf("# Wallet dump created by Altcoin %s\n", PACKAGE_VERSION);
     file << strprintf("# * Created on %s\n", FormatISO8601DateTime(GetTime()));
     file << strprintf("# * Best block at time of backup was %i (%s),\n", wallet.GetLastBlockHeight(), wallet.GetLastBlockHash().ToString());
     int64_t block_time = 0;
